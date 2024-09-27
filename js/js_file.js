d2cc646ffb602693beded1b5b1629c2171b58793
@oshhomaho <oshhomaho@gmail.com>
2024-09-27 22:30:20 +0300

    button+icon

---
+++ js/js_file.js
@@ -0,0 +1,8 @@
+function toggleImage() {
+    var img = document.getElementById("invise");
+    if (img.style.display === "none") {
+        img.style.display = "block";
+    } else {
+        img.style.display = "none";
+    }
+}
